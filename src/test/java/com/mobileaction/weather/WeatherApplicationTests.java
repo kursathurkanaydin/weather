eacbac994b5df57c1baa347c8e53f921c50a86fe
kursathurkanaydin <kursat.aydin@hacettepe.edu.tr>
2026-07-21 09:37:37 +0300

    feat(weather): create project

---
+++ src/test/java/com/mobileaction/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobileaction.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
