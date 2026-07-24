f697ca9eb1f3b94acf12b3f1ae991c779cdb772f
kursathurkanaydin <kursat.aydin@hacettepe.edu.tr>
2026-07-24 09:58:41 +0300

    feat(air-pollution): add equals and hashCode methods to AirPolluiton entity

--- src/main/java/com/mobileaction/weather/model/AirPollution.java
+++ src/main/java/com/mobileaction/weather/model/AirPollution.java
@@ -6,6 +6,7 @@ import lombok.*;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "air_pollutions")
@@ -29,4 +30,21 @@ public class AirPollution
 
     @Column(name = "date")
     private LocalDate date;
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
+        AirPollution that = (AirPollution) o;
+        return Objects.equals(id, that.id) && Objects.equals(city, that.city) && Objects.equals(date, that.date);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(id, city, date);
+    }
 }
